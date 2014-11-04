bf9c673c95cde737ee3750bef102f37f9191528f
hoxis <liuhao1960@gmail>
2014-11-04 16:33:31 +0800

    1、为每个自条目添加事件响应；2、为“手机防盗”添加长按修改名称功能

--- src/com/liuhao/mobilesafe/adapter/MainUIAdapter.java
+++ src/com/liuhao/mobilesafe/adapter/MainUIAdapter.java
@@ -3,6 +3,7 @@ package com.liuhao.mobilesafe.adapter;
 import com.liuhao.mobilesafe.R;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,34 +15,50 @@ import android.widget.TextView;
 public class MainUIAdapter extends BaseAdapter {
 	
 	private static final String TAG = "MainUIAdapter";
-	private Context context;
+	private Context context;//用于接收传递过来的Context对象
 	private LayoutInflater inflater;
 	private static ImageView iv_icon;
 	private static TextView tv_name;
+	private SharedPreferences sp;
 	
 	public MainUIAdapter(Context context) {
 		this.context = context;
 		inflater = LayoutInflater.from(context);
+		sp = context.getSharedPreferences("config", Context.MODE_PRIVATE);
 	}
 
+	// 数据源，子条目的文本内容
 	private static String[] names = { "手机防盗", "通讯卫士", "软件管理", "任务管理", "上网管理",
 			"手机杀毒", "系统优化", "高级工具", "设置中心" };
 
+	// 数据源，子条目的对应图片
 	private static int[] icons = { R.drawable.widget05, R.drawable.widget02,
 			R.drawable.widget01, R.drawable.widget07, R.drawable.widget05,
 			R.drawable.widget04, R.drawable.widget06, R.drawable.widget03,
 			R.drawable.widget08 };
 
+	/**
+	 * How many items are in the data set represented by this Adapter.
+	 * 在此适配器中所代表的数据集中的条目数
+	 */
 	@Override
 	public int getCount() {
 		return names.length;
 	}
 
+	/**
+	 * Get the data item associated with the specified position in the data set.
+	 * 获取数据集中与指定索引对应的数据项
+	 */
 	@Override
 	public Object getItem(int position) {
 		return position;
 	}
 
+	/**
+	 * Get the row id associated with the specified position in the list.
+	 * 取在列表中与指定索引对应的行id
+	 */
 	@Override
 	public long getItemId(int position) {
 		return position;
@@ -54,12 +71,23 @@ public class MainUIAdapter extends BaseAdapter {
 		//GridView 控件bug
 		// 使用静态变量引用来减少内存中申请的引用的个数
 		Log.i(TAG, "getView" + position);
+		
+		// 得到布局文件对应的View对象
+		// inflate()方法一般接收两个参数，第一个参数就是要加载的布局id，
+		// 第二个参数是指给该布局的外部再嵌套一层父布局，如果不需要就直接传null。
 		View view = inflater.inflate(R.layout.mainscreen_item, null);
 		iv_icon = (ImageView) view.findViewById(R.id.iv_main_icon);
 		tv_name = (TextView) view.findViewById(R.id.tv_main_name);
 		
 		iv_icon.setImageResource(icons[position]);
 		tv_name.setText(names[position]);
+		if(position == 0){
+			// 从SharedPreferences获取条目的文本内容
+			String name = sp.getString("lost_name", null);
+			if(!"".equals(name) && null != name){
+				tv_name.setText(name);
+			}
+		}
 		
 		return view;
 	}
--- src/com/liuhao/mobilesafe/ui/MainActivity.java
+++ src/com/liuhao/mobilesafe/ui/MainActivity.java
@@ -4,28 +4,94 @@ import com.liuhao.mobilesafe.R;
 import com.liuhao.mobilesafe.adapter.MainUIAdapter;
 
 import android.app.Activity;
+import android.app.AlertDialog.Builder;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnItemClickListener {
 
 	private static final String TAG = "MainActivity";
 	private GridView gv_main;
 	private MainUIAdapter adapter;
+	// 用来持久化一些配置信息
+	private SharedPreferences sp;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		//Set the activity content from a layout resource. The resource will be inflated, adding all top-level views to the activity
 		setContentView(R.layout.mainscreen);
+		sp = this.getSharedPreferences("config", Context.MODE_PRIVATE);// 初始化
+		
 		gv_main = (GridView) this.findViewById(R.id.gv_main);
 		adapter = new MainUIAdapter(this);
 		gv_main.setAdapter(adapter);
 		gv_main.setOnItemClickListener(this);
+		
+		gv_main.setOnItemLongClickListener(new OnItemLongClickListener() {
+
+			@Override
+			public boolean onItemLongClick(AdapterView<?> parent, final View view,
+					int position, long id) {
+				
+				// 长按只对第一个条目有效
+				if(position == 0){
+					Builder builder = new Builder(MainActivity.this);
+					builder.setTitle("设置");
+					builder.setMessage("请输入要更改的内容");
+					
+					final EditText et = new EditText(MainActivity.this);
+					et.setHint("请输入内容，长度在1-4之间");
+					builder.setView(et);
+					
+					builder.setPositiveButton("确定", new OnClickListener() {
+						
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							String name = et.getText().toString().trim();// 获取用户的输入值
+							if("".equals(name)){
+								Toast.makeText(getApplicationContext(), "输入为空无效哦~", Toast.LENGTH_LONG).show();
+								return;
+							}else if(name.length() > 4){
+								Toast.makeText(getApplicationContext(), "输入超过4个字啦~", Toast.LENGTH_LONG).show();
+								return;
+							}else{
+								Editor editor = sp.edit();// 用于修改一个SharedPreferences的值
+								editor.putString("lost_name", name);
+								// 完成数据的提交
+								editor.commit();
+								TextView tv = (TextView) view.findViewById(R.id.tv_main_name);
+								tv.setText(name);// 更新条目的文本内容
+							}
+						}
+					});
+					
+					builder.setNegativeButton("取消", new OnClickListener() {
+						
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+						}
+					});
+					
+					builder.create().show();
+				}
+				return false;
+			}
+			
+		});
 		
 	}
 
