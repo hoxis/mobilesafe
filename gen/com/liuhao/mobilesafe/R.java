27d280b619c435b3b6b05e63ec049871b10832f8
hoxis <liuhao1960@gmail>
2014-11-15 23:52:11 +0800

    进入手机防盗界面设置密码，输入密码 具体功能的实现

--- gen/com/liuhao/mobilesafe/R.java
+++ gen/com/liuhao/mobilesafe/R.java
@@ -41,21 +41,27 @@ public final class R {
         public static final int widget08=0x7f02000d;
     }
     public static final class id {
-        public static final int action_settings=0x7f090007;
+        public static final int action_settings=0x7f09000c;
+        public static final int bt_first_dialog_cancel=0x7f090003;
+        public static final int bt_first_dialog_confirm=0x7f090002;
+        public static final int bt_normal_dialog_cancel=0x7f090009;
+        public static final int bt_normal_dialog_confirm=0x7f090008;
         public static final int et_first_entry_pwd=0x7f090000;
         public static final int et_first_entry_pwd_confirm=0x7f090001;
-        public static final int gv_main=0x7f090002;
-        public static final int iv_main_icon=0x7f090003;
-        public static final int ll_splash_main=0x7f090005;
-        public static final int tv_main_name=0x7f090004;
-        public static final int tv_splash_version=0x7f090006;
+        public static final int et_normal_entry_pwd=0x7f090007;
+        public static final int gv_main=0x7f090004;
+        public static final int iv_main_icon=0x7f090005;
+        public static final int ll_splash_main=0x7f09000a;
+        public static final int tv_main_name=0x7f090006;
+        public static final int tv_splash_version=0x7f09000b;
     }
     public static final class layout {
         public static final int first_entry_dialog=0x7f030000;
         public static final int main=0x7f030001;
         public static final int mainscreen=0x7f030002;
         public static final int mainscreen_item=0x7f030003;
-        public static final int splash=0x7f030004;
+        public static final int normal_entry_dialog=0x7f030004;
+        public static final int splash=0x7f030005;
     }
     public static final class menu {
         public static final int splash=0x7f080000;
--- src/com/liuhao/mobilesafe/ui/LostProtectedActivity.java
+++ src/com/liuhao/mobilesafe/ui/LostProtectedActivity.java
@@ -6,14 +6,22 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LostProtectedActivity extends Activity {
+public class LostProtectedActivity extends Activity implements OnClickListener {
 
 	private static final String TAG = "LostProtectedActivity";
 	private SharedPreferences sp;
 	private Dialog dialog;
+	private EditText et_pwd;
+	private EditText et_pwd_confirm;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +30,32 @@ public class LostProtectedActivity extends Activity {
 		// 判读用户是否已经设置了密码
 		if (isPwdSetup()) {
 			Log.i(TAG, "设置了密码，弹出输入密码的对话框");
+			showNormalEntryDialog();
 		} else {
 			Log.i(TAG, "未设置密码，弹出设置密码对话框");
 			showFirstEntryDialog();
 		}
+	}
+
+	/**
+	 * 正常登录的对话框
+	 * 
+	 */
+	private void showNormalEntryDialog() {
+		dialog = new Dialog(this, R.style.MyDialog);
+		View view = View.inflate(this, R.layout.normal_entry_dialog, null);
+		et_pwd = (EditText) view.findViewById(R.id.et_normal_entry_pwd);
+		Button bt_confirm = (Button) view.findViewById(R.id.bt_normal_dialog_confirm);
+		Button bt_cancel = (Button) view.findViewById(R.id.bt_normal_dialog_cancel);
+		
+		// 设置按钮对应的点击事件
+		bt_confirm.setOnClickListener(this);
+		bt_cancel.setOnClickListener(this);
+		
+		dialog.setContentView(view);
+		dialog.setCanceledOnTouchOutside(false);// 设置dialog不可以点击其他地方时消失
+//		dialog.setCancelable(false);// 设置dialog不可以点返回键时消失
+		dialog.show();
 	}
 
 	/**
@@ -34,7 +64,20 @@ public class LostProtectedActivity extends Activity {
 	 */
 	private void showFirstEntryDialog() {
 		dialog = new Dialog(this, R.style.MyDialog);
-		dialog.setContentView(R.layout.first_entry_dialog);// 设置要显示的内容
+//		dialog.setContentView(R.layout.first_entry_dialog);// 设置要显示的内容
+		View view = View.inflate(this, R.layout.first_entry_dialog, null);
+		et_pwd = (EditText) view.findViewById(R.id.et_first_entry_pwd);
+		et_pwd_confirm = (EditText) view.findViewById(R.id.et_first_entry_pwd_confirm);
+		Button bt_confirm = (Button) view.findViewById(R.id.bt_first_dialog_confirm);
+		Button bt_cancel = (Button) view.findViewById(R.id.bt_first_dialog_cancel);
+		
+		// 设置按钮对应的点击事件
+		bt_confirm.setOnClickListener(this);
+		bt_cancel.setOnClickListener(this);
+		
+		dialog.setContentView(view);
+		dialog.setCanceledOnTouchOutside(false);// 设置dialog不可以点击其他地方时消失
+//		dialog.setCancelable(false);// 设置dialog不可以点返回键时消失
 		dialog.show();
 	}
 
@@ -53,6 +96,58 @@ public class LostProtectedActivity extends Activity {
 			} else {
 				return true;
 			}
+		}
+	}
+
+	@Override
+	public void onClick(View view) {
+		switch(view.getId()){
+		// 点击取消
+		case R.id.bt_first_dialog_cancel:
+			dialog.dismiss();
+			break;
+		case R.id.bt_first_dialog_confirm:
+			String pwd = et_pwd.getText().toString().trim();
+			String pwd_confirm = et_pwd_confirm.getText().toString().trim();
+			
+			// 输入的密码中包好空值
+			if("".equals(pwd) || "".equals(pwd_confirm)){
+				Toast.makeText(getApplicationContext(), "输入不能为空！", Toast.LENGTH_LONG).show();
+				return;
+			}else{
+				if(pwd.equals(pwd_confirm)){
+					Editor editor = sp.edit();
+					editor.putString("password", pwd);
+					editor.commit();
+				}
+				// 两次输入不一致
+				else{
+					Toast.makeText(getApplicationContext(), "两次输入密码不相同！", Toast.LENGTH_LONG).show();
+					et_pwd_confirm.selectAll();// 用户输入错误后，对文本进行全选，方便用户进行删除重新输入
+					return;
+				}
+			}
+			dialog.dismiss();
+			break;
+		case R.id.bt_normal_dialog_cancel:
+			dialog.dismiss();
+			break;
+		case R.id.bt_normal_dialog_confirm:
+			String input_pwd = et_pwd.getText().toString();
+			if("".equals(input_pwd)){
+				Toast.makeText(getApplicationContext(), "输入不能为空！", Toast.LENGTH_LONG).show();
+				return;
+			}else{
+				String password = sp.getString("password", "");
+				if(!password.equals(input_pwd)){
+					Toast.makeText(getApplicationContext(), "输入密码不正确，请重新输入！", Toast.LENGTH_LONG).show();
+					et_pwd.selectAll();// 用户输入错误后，对文本进行全选，方便用户进行删除重新输入
+					return;
+				}
+			}
+			Log.i(TAG, "加载手机防盗主界面");
+			dialog.dismiss();
+			break;
 		}
 	}
 
