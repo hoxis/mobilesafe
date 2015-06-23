1630970325786bb785476633e63fe2cead9d4cfe
hoxis <liuhao1960@gmail.com>
2015-06-23 23:16:46 +0800

    来电显示风格可修改。

--- src/com/liuhao/mobilesafe/service/AttributionService.java
+++ src/com/liuhao/mobilesafe/service/AttributionService.java
@@ -1,10 +1,9 @@
 package com.liuhao.mobilesafe.service;
 
-import com.liuhao.mobilesafe.R;
-import com.liuhao.mobilesafe.engine.NumberAttributionService;
-
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.PixelFormat;
 import android.os.IBinder;
 import android.telephony.PhoneStateListener;
@@ -13,91 +12,108 @@ import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.WindowManager.LayoutParams;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.liuhao.mobilesafe.R;
+import com.liuhao.mobilesafe.engine.NumberAttributionService;
 
 public class AttributionService extends Service {
 
-	public static final String TAG = "AttributionService";
-	private TelephonyManager manager;
-	private MyPhoneListener listener;
-	private WindowManager windowManager;
-	private View view;
+    public static final String TAG = "AttributionService";
+    private TelephonyManager manager;
+    private MyPhoneListener listener;
+    private WindowManager windowManager;
+    private View view;
+    private SharedPreferences sp;
 
-	@Override
-	public IBinder onBind(Intent intent) {
-		return null;
-	}
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 
-	@Override
-	public void onCreate() {
-		super.onCreate();
-		windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
-		listener = new MyPhoneListener();
-		// 注册系统电话管理服务的监听器
-		manager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
-		manager.listen(listener, PhoneStateListener.LISTEN_CALL_STATE);
-	}
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sp = getSharedPreferences("config", Context.MODE_PRIVATE);
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        listener = new MyPhoneListener();
+        // 注册系统电话管理服务的监听器
+        manager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+        manager.listen(listener, PhoneStateListener.LISTEN_CALL_STATE);
+    }
 
-	private class MyPhoneListener extends PhoneStateListener {
+    private class MyPhoneListener extends PhoneStateListener {
 
-		// 电话状态发生改变时调用的方法
-		@Override
-		public void onCallStateChanged(int state, String incomingNumber) {
-			super.onCallStateChanged(state, incomingNumber);
-			switch (state) {
-			case TelephonyManager.CALL_STATE_IDLE:// 处于静止状态，没有呼叫
-				if(view != null){
-					windowManager.removeView(view);
-					view = null;
-				}
-				break;
-			case TelephonyManager.CALL_STATE_RINGING:// 响铃状态
-				Log.i(TAG, "来电号码为：" + incomingNumber);
-				String attribution = NumberAttributionService
-						.getAttribution(incomingNumber);
-				Log.i(TAG, "归属地为：" + attribution);
-				// Toast.makeText(getApplicationContext(), "归属地为：" +
-				// attribution, Toast.LENGTH_LONG).show();
-				showAttribution(attribution);
-				break;
-			case TelephonyManager.CALL_STATE_OFFHOOK:// 接通电话状态
-				
-				break;
-			}
-		}
+        // 电话状态发生改变时调用的方法
+        @Override
+        public void onCallStateChanged(int state, String incomingNumber) {
+            super.onCallStateChanged(state, incomingNumber);
+            switch (state) {
+                case TelephonyManager.CALL_STATE_IDLE:// 处于静止状态，没有呼叫
+                    if (view != null) {
+                        windowManager.removeView(view);
+                        view = null;
+                    }
+                    break;
+                case TelephonyManager.CALL_STATE_RINGING:// 响铃状态
+                    Log.i(TAG, "来电号码为：" + incomingNumber);
+                    String attribution = NumberAttributionService
+                            .getAttribution(incomingNumber);
+                    Log.i(TAG, "归属地为：" + attribution);
+                    // Toast.makeText(getApplicationContext(), "归属地为：" +
+                    // attribution, Toast.LENGTH_LONG).show();
+                    showAttribution(attribution);
+                    break;
+                case TelephonyManager.CALL_STATE_OFFHOOK:// 接通电话状态
 
-		/**
-		 * 显示归属地信息
-		 * 
-		 * @param attribution
-		 */
-		private void showAttribution(String attribution) {
-			WindowManager.LayoutParams params = new LayoutParams();
-			params.height = WindowManager.LayoutParams.WRAP_CONTENT;
-			params.width = WindowManager.LayoutParams.WRAP_CONTENT;
-			params.format = PixelFormat.TRANSLUCENT;
-			params.type = WindowManager.LayoutParams.TYPE_TOAST;
-			params.setTitle("Toast");
-			params.flags = WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
-					| WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
-					| WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE;
-			
-			view = View.inflate(getApplicationContext(), R.layout.show_attribution, null);
-			TextView tv_attribution = (TextView) view.findViewById(R.id.tv_attribution);
-			tv_attribution.setText(attribution);
-			tv_attribution.setTextSize(24);
-			
-			windowManager.addView(view, params);
-		}
+                    break;
+            }
+        }
 
-	}
+        /**
+         * 显示归属地信息
+         *
+         * @param attribution
+         */
+        private void showAttribution(String attribution) {
+            WindowManager.LayoutParams params = new LayoutParams();
+            params.height = WindowManager.LayoutParams.WRAP_CONTENT;
+            params.width = WindowManager.LayoutParams.WRAP_CONTENT;
+            params.format = PixelFormat.TRANSLUCENT;
+            params.type = WindowManager.LayoutParams.TYPE_TOAST;
+            params.setTitle("Toast");
+            params.flags = WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                    | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                    | WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE;
 
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		// 取消注册系统电话管理服务的监听器
-		manager.listen(listener, PhoneStateListener.LISTEN_NONE);
-		listener = null;
-	}
+            view = View.inflate(getApplicationContext(), R.layout.show_attribution, null);
+            TextView tv_attribution = (TextView) view.findViewById(R.id.tv_attribution);
+
+            LinearLayout ll_attribution = (LinearLayout) view.findViewById(R.id.ll_attribution);
+            int background = sp.getInt("background", 0);
+            if (background == 0) {
+                ll_attribution.setBackgroundResource(R.drawable.call_locate_gray);
+            } else if (background == 1) {
+                ll_attribution.setBackgroundResource(R.drawable.call_locate_orange);
+            } else if (background == 1) {
+                ll_attribution.setBackgroundResource(R.drawable.call_locate_green);
+            }
+
+            tv_attribution.setText(attribution);
+            tv_attribution.setTextSize(24);
+
+            windowManager.addView(view, params);
+        }
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        // 取消注册系统电话管理服务的监听器
+        manager.listen(listener, PhoneStateListener.LISTEN_NONE);
+        listener = null;
+    }
 
 }
--- src/com/liuhao/mobilesafe/ui/AtoolsActivity.java
+++ src/com/liuhao/mobilesafe/ui/AtoolsActivity.java
@@ -1,14 +1,12 @@
 package com.liuhao.mobilesafe.ui;
 
-import java.io.File;
-
-import com.liuhao.mobilesafe.R;
-import com.liuhao.mobilesafe.engine.DownloadFileTask;
-import com.liuhao.mobilesafe.service.AttributionService;
-
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Environment;
@@ -22,112 +20,152 @@ import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.liuhao.mobilesafe.R;
+import com.liuhao.mobilesafe.engine.DownloadFileTask;
+import com.liuhao.mobilesafe.service.AttributionService;
+
+import java.io.File;
+
 public class AtoolsActivity extends Activity implements OnClickListener {
 
-	protected static final int SUCCESS = 11;
-	protected static final int ERROR = 10;
-	private TextView tv_query;
-	private ProgressDialog pd;
-	private TextView tv_atools_attribution;
-	private CheckBox cb_atools_attribution;
-	private Intent serviceIntent;
-	
-	private Handler handler = new Handler(){
+    protected static final int SUCCESS = 11;
+    protected static final int ERROR = 10;
+    private TextView tv_query;
+    private ProgressDialog pd;
+    private TextView tv_atools_attribution;
+    private CheckBox cb_atools_attribution;
+    private Intent serviceIntent;
+    private TextView tv_atools_select_bg;
+    private SharedPreferences sp;
 
-		@Override
-		public void handleMessage(Message msg) {
-			super.handleMessage(msg);
-			switch (msg.what) {
-			case SUCCESS:
-				Toast.makeText(getApplicationContext(), "下载数据库成功", Toast.LENGTH_LONG).show();
-				break;
-			case ERROR:
-				Toast.makeText(getApplicationContext(), "下载数据库失败", Toast.LENGTH_LONG).show();
-				break;
-			}
-		}
-		
-	};
+    private Handler handler = new Handler() {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.atools);
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case SUCCESS:
+                    Toast.makeText(getApplicationContext(), "下载数据库成功", Toast.LENGTH_LONG).show();
+                    break;
+                case ERROR:
+                    Toast.makeText(getApplicationContext(), "下载数据库失败", Toast.LENGTH_LONG).show();
+                    break;
+            }
+        }
 
-		tv_atools_attribution = (TextView) this.findViewById(R.id.tv_atools_attribution);
-		cb_atools_attribution = (CheckBox) this.findViewById(R.id.cb_atools_attribution);
-		serviceIntent = new Intent(this, AttributionService.class);
-		cb_atools_attribution.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-			
-			@Override
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-				if(isChecked){
-					startService(serviceIntent);
-					tv_atools_attribution.setTextColor(Color.WHITE);
-					tv_atools_attribution.setText("显示来电归属地服务已开启");
-				} else{
-					stopService(serviceIntent);
-					tv_atools_attribution.setTextColor(Color.RED);
-					tv_atools_attribution.setText("显示来电归属地服务未开启");
-				}
-			}
-		});
-		
-		tv_query = (TextView) this.findViewById(R.id.tv_atools_query);
-		tv_query.setOnClickListener(this);
-	}
+    };
 
-	@Override
-	public void onClick(View v) {
-		switch (v.getId()) {
-		case R.id.tv_atools_query:
-			
-			// 判断归属地数据库是否存在
-			if(isDBExist()){
-				Intent intent = new Intent(this, QueryNumberActivity.class);
-				startActivity(intent);
-			}
-			else{
-				// 提示用户下载数据库
-				pd = new ProgressDialog(this);
-				pd.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);// 默认情况下不显示进度，这个设置用于显示进度
-				pd.setMessage("正在下载数据库...");
-				pd.setCancelable(false);
-				pd.setCanceledOnTouchOutside(false);
-				pd.show();
-				// 下载数据库
-				new Thread(){
-					public void run() {
-						String path = getResources().getString(R.string.dbadressurl);
-						String filepath = Environment.getExternalStorageDirectory().getPath() + "/address.db";
-						try {
-							DownloadFileTask.getFile(path, filepath, pd);
-							pd.dismiss();
-							Message msg = new Message();
-							msg.what = SUCCESS;
-							handler.sendMessage(msg);
-						} catch (Exception e) {
-							e.printStackTrace();
-							pd.dismiss();
-							Message msg = new Message();
-							msg.what = ERROR;
-							handler.sendMessage(msg);
-						}
-					};
-				}.start();
-			}
-			break;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.atools );
 
-		}
-	}
+        sp = getSharedPreferences("config", Context.MODE_PRIVATE);
 
-	/**
-	 * 判断归属地数据库是否存在
-	 * 
-	 * @return
-	 */
-	public boolean isDBExist() {
-		File file = new File(Environment.getExternalStorageDirectory().getPath() + "/address.db");
-		return file.exists();
-	}
+        tv_atools_select_bg = (TextView) this.findViewById(R.id.tv_atools_select_bg);
+        tv_atools_select_bg.setOnClickListener(this);
+
+        tv_atools_attribution = (TextView) this.findViewById(R.id.tv_atools_attribution);
+        cb_atools_attribution = (CheckBox) this.findViewById(R.id.cb_atools_attribution);
+        serviceIntent = new Intent(this, AttributionService.class);
+        cb_atools_attribution.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    startService(serviceIntent);
+                    tv_atools_attribution.setTextColor(Color.WHITE);
+                    tv_atools_attribution.setText("显示来电归属地服务已开启");
+                } else {
+                    stopService(serviceIntent);
+                    tv_atools_attribution.setTextColor(Color.RED);
+                    tv_atools_attribution.setText("显示来电归属地服务未开启");
+                }
+            }
+        });
+
+        tv_query = (TextView) this.findViewById(R.id.tv_atools_query);
+        tv_query.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_atools_query:
+
+                // 判断归属地数据库是否存在
+                if (isDBExist()) {
+                    Intent intent = new Intent(this, QueryNumberActivity.class);
+                    startActivity(intent);
+                } else {
+                    // 提示用户下载数据库
+                    pd = new ProgressDialog(this);
+                    pd.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);// 默认情况下不显示进度，这个设置用于显示进度
+                    pd.setMessage("正在下载数据库...");
+                    pd.setCancelable(false);
+                    pd.setCanceledOnTouchOutside(false);
+                    pd.show();
+                    // 下载数据库
+                    new Thread() {
+                        public void run() {
+                            String path = getResources().getString(R.string.dbadressurl);
+                            String filepath = Environment.getExternalStorageDirectory().getPath() + "/address.db";
+                            try {
+                                DownloadFileTask.getFile(path, filepath, pd);
+                                pd.dismiss();
+                                Message msg = new Message();
+                                msg.what = SUCCESS;
+                                handler.sendMessage(msg);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                                pd.dismiss();
+                                Message msg = new Message();
+                                msg.what = ERROR;
+                                handler.sendMessage(msg);
+                            }
+                        }
+
+                        ;
+                    }.start();
+                }
+                break;
+
+            case R.id.tv_atools_select_bg:
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("归属地提示显示风格");
+                String[] items = new String[]{"半透明", "活力橙", "苹果绿"};
+                builder.setSingleChoiceItems(items, 0, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //将用户选择的风格存储到SharedPreference中
+                        SharedPreferences.Editor editor = sp.edit();
+                        editor.putInt("background",which);
+                        editor.commit();
+                    }
+                });
+
+                builder.setPositiveButton("确定",new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                builder.create().show();
+
+                break;
+
+        }
+    }
+
+    /**
+     * 判断归属地数据库是否存在
+     *
+     * @return
+     */
+    public boolean isDBExist() {
+        File file = new File(Environment.getExternalStorageDirectory().getPath() + "/address.db");
+        return file.exists();
+    }
 }
